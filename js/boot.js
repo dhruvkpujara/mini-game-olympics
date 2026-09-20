32e3ec2cf7dccf961d72fdef8a8f694472a4b3b4
dhruvkpujara <dhruvkpujara@gmail.com>
2026-09-20 17:20:38 +0530

    Bump main cache after gameplay fixes

--- js/boot.js
+++ js/boot.js
@@ -37,7 +37,7 @@
     ['js/ui.js?v=5','Loading interface...'],
     ['js/game_state.js?v=3','Loading game state...'],
     ['js/tournament.js?v=1','Loading tournament system...'],
-    ['js/main.js?v=19','Starting 3D engine...']
+    ['js/main.js?v=20','Starting 3D engine...']
   ];
 
   async function start(){
